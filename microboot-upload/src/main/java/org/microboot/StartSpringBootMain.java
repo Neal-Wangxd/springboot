bece164ccf2a520aff578e3be79c7c44ea268770
Neal-Wangxd <wangxd_work@163.com>
2018-01-16 15:40:45 +0800

    主启动类

---
+++ microboot-upload/src/main/java/org/microboot/StartSpringBootMain.java
@@ -0,0 +1,19 @@
+package org.microboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/** 
+* @author 作者 wxd  
+* @version 1.0
+* E-mail: wangxudong@jetsen.cn
+* 创建时间：2018年1月5日 下午4:35:10 
+* 类说明 
+*/
+@SpringBootApplication
+public class StartSpringBootMain {
+    public static void main(String[] args) {
+        SpringApplication.run(StartSpringBootMain.class, args);
+    }
+}
+ 
