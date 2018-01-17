2ac860f61313331c370dfe9a573f1722eb89add7
Neal-Wangxd <wangxd_work@163.com>
2018-01-17 14:26:03 +0800

    member业务操作接口

---
+++ microboot-interceptor/src/main/java/com/microboot/service/MemberService.java
@@ -0,0 +1,16 @@
+package com.microboot.service;
+
+import com.microboot.bean.Member;
+
+/** 
+* @author 作者 wxd  
+* @version 1.0
+* E-mail: wangxudong@jetsen.cn
+* 创建时间：2018年1月17日 上午11:50:30 
+* 类说明 
+*/
+public interface MemberService {
+
+	public Member getMember(String mid);
+}
+ 
