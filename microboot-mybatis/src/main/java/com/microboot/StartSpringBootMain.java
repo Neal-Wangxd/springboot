d1ee97c01ae5a80c1af05a9658039a4c7259680b
Neal-Wangxd <wangxd_work@163.com>
2018-01-19 10:16:19 +0800

    springboot启动类

---
+++ microboot-mybatis/src/main/java/com/microboot/StartSpringBootMain.java
@@ -0,0 +1,21 @@
+package com.microboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/** 
+* @author 作者 wxd  
+* @version 1.0
+* E-mail: wangxudong@jetsen.cn
+* 创建时间：2018年1月5日 下午4:35:10 
+* 类说明 
+*/
+@SpringBootApplication
+@EnableTransactionManagement
+public class StartSpringBootMain {
+    public static void main(String[] args) {
+        SpringApplication.run(StartSpringBootMain.class, args);
+    }
+}
+ 
