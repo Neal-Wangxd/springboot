042c87d7a6fa3ad3e7c1f3602fc42425992cfd0f
Neal-Wangxd <wangxd_work@163.com>
2018-01-30 10:18:21 +0800

    Member控制类

---
+++ microboot-restful-consumer/src/main/java/com/microboot/controller/MemberConsumerController.java
@@ -0,0 +1,54 @@
+package com.microboot.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestTemplate;
+
+import com.microboot.bean.Member;
+
+/** 
+* @author 作者 wxd  
+* @version 1.0
+* E-mail: wangxudong@jetsen.cn
+* 创建时间：2018年1月23日 下午3:25:54 
+* 类说明 
+*/
+@Controller
+public class MemberConsumerController extends AbstractBaseController {
+
+	@Resource
+	private RestTemplate restTemplate;
+	
+	@RequestMapping(value="/get")
+	public String getMember(long mid,Model model){
+		Member member = this.restTemplate.getForObject(
+				"http://localhost:8088/restful-provider/member/get/"+ mid, Member.class);
+		List<Member> members = new ArrayList<>();
+		members.add(member);
+		model.addAttribute("memberList",members);
+		
+		return "member/member_show";
+	}
+	
+	@RequestMapping(value = "/add")
+	public String addMember(Model model){
+		return "member/member_add";
+	}
+	
+	@RequestMapping(value="/addDo",method=RequestMethod.GET)
+	@ResponseBody
+	public Object addMemberDo(Member member){
+		Boolean flag = this.restTemplate.getForObject(
+				"http://localhost:8088/restful-provider/member/add"+ member, boolean.class);
+		return flag ;
+	}
+}
+ 
