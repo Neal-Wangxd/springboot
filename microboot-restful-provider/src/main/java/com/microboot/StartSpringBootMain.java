51d01c3749a8efebe92637b20168a75ef84e0281
Neal-Wangxd <wangxd_work@163.com>
2018-01-30 10:23:17 +0800

    启动类

---
+++ microboot-restful-provider/src/main/java/com/microboot/StartSpringBootMain.java
@@ -0,0 +1,19 @@
+package com.microboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/** 
+* @author 作者 wxd  
+* @version 1.0
+* E-mail: wangxudong@jetsen.cn
+* 创建时间：2018年1月23日 下午2:05:25 
+* 类说明 
+*/
+@SpringBootApplication
+public class StartSpringBootMain {
+    public static void main(String[] args) {
+        SpringApplication.run(StartSpringBootMain.class, args);
+    }
+}
+ 
